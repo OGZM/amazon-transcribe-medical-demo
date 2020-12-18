937d7b819654ae0597d06dd4f59447199938de8b
OGZM <38015097+OGZM@users.noreply.github.com>
2020-12-18 19:23:57 -0400

    Update aws-credentials.js
    Updating the aws-credentials.js file

--- src/aws-credential.js
+++ src/aws-credential.js
@@ -1,5 +1,5 @@
 export const credential = {
-    region: 'us-west-2', 
-    accessKeyId: 'YOUR_AWS_ACCESS_KEY', 
-    secretAccessKey: 'YOUR_AWS_SECRET_KEY'
+    region: 'us-east-1', 
+    accessKeyId: 'AKIAIK6E6YRNC3LU4U2Q', 
+    secretAccessKey: 'k/pxVxKFmuIYM+mUMz9t/KAt8bMhxY7UFFWGd4ut'
 }
