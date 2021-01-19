eae2192307ed4f1558e7e72f34f6c0563f463237
OGZM <38015097+OGZM@users.noreply.github.com>
2021-01-19 13:04:10 -0400

    delete key

--- src/aws-credential.js
+++ src/aws-credential.js
@@ -1,5 +1,5 @@
 export const credential = {
     region: 'us-east-1', 
-    accessKeyId: 'AKIAIK6E6YRNC3LU4U2Q', 
-    secretAccessKey: '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'
+    accessKeyId: '', 
+    secretAccessKey: 'k/+mUMz9t/'
 }
